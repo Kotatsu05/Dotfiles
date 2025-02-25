f5cf9afb251c79f04294a133b47b902d6f007379
Kotatsu <kotatsu.0550@gmail.com>
2025-02-25 18:27:26 +0100

    tmux

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#Install needed Packages
 sudo pacman -S polkit --noconfirm
 sudo pacman -S picom --noconfirm
 sudo pacman -S feh --noconfirm
@@ -15,3 +16,16 @@ sudo pacman -S libxft --noconfirm
 sudo pacman -S libxinerama --noconfirm
 sudo pacman -S stow --noconfirm
 sudo pacman -S starship --noconfirm
+sudo pacman -S zip --noconfirm
+sudo pacman -S unzip -noconfirm
+
+#Make shure git is installed
+sudo pacman -S git --noconfirm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#Install gtk Theme
+sudo mkdir -p /usr/share/themes
+sudp cp -r themes/Tokyonight-Dark /usr/share/themes
+
+sudo mkdir -p /usr/share/icons
+sudp cp -r themes/dracula-icons-main /usr/share/icons
