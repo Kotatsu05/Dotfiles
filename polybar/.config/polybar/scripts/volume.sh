23b42ebf530b5d6db469ce74f3ab954ee73462e0
Kotatsu <kotatsu.0550@gmail.com>
2025-02-28 19:14:22 +0100

    polybar volume fix

--- polybar/.config/polybar/scripts/volume.sh
+++ polybar/.config/polybar/scripts/volume.sh
@@ -5,6 +5,11 @@ volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oP '\d+' | tr -d '\n')
 # Convert the value to a percentage
 #percentage=$(echo "scale=0; $volume * 100" | bc)
 
+if [[ "${volume:0:1}" == "0" ]]; then
+    # Remove the first character
+    volume="${volume:1}"
+fi
+
 muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oP 'MUTED')
 if [ -n "$muted" ]; then
     echo "MUTED"
