2901d49f8aebd4eec1874935b9a18e6fdbd10aea
Kotatsu <kotatsu.0550@gmail.com>
2025-08-09 17:26:17 +0200

    24H

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo pacman -S polkit feh fzf zsh neovim ttf-firacode-nerd brightnessctl stow starship zip unzip playerctl fortune-mod git --noconfirm
+
+sudo mkdir -p /usr/share/themes
+sudo cp -r themes/Tokyonight-Dark /usr/share/themes/
+
+sudo mkdir -p /usr/share/icons
+sudp cp -r /themes/dracula-icons/main /usr/share/icons/
